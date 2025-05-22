6dd6cdb8f2f69d87a378810fb313ee48b2c4622c
DG-Lev <l.chekur@outlook.com>
2025-05-22 15:50:50 -0400

    Updated ProductList and CartItem components

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { addItem, updateQuantity, removeItem } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
@@ -27,7 +27,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
   
   const handleIncrement = (item) => {
-    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+    dispatch(addItem(item));
   };
 
   const handleDecrement = (item) => {
